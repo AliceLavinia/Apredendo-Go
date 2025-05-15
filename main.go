7c166c2e00803fada975f64d5f4068bff867d836
AliceLavinia <alicelhcv@gmail.com>
2025-05-15 19:05:14 -0300

    Update main.go

--- main.go
+++ main.go
@@ -8,21 +8,22 @@ func main() {
 	var altura float64 = 1.55
 	var casada bool = true
     
-	fmt.Println("Nome:",nome)
-	fmt.Scan(&nome)
+	// fmt.Println("Nome:",nome)
+	// fmt.Scan(&nome)
 
-	fmt.Println("Idade: ",idade)
-    fmt.Scan(&idade)
+	// fmt.Println("Idade: ",idade)
+    // fmt.Scan(&idade)
 
-	fmt.Println("Altura: ",altura)
-	fmt.Scan(&altura)
+	// fmt.Println("Altura: ",altura)
+	// fmt.Scan(&altura)
 
-	fmt.Println("Casada: ",casada)
-	fmt.Scan(&casada)
+	// fmt.Println("Casada: ",casada)
+	// fmt.Scan(&casada)
     
-    fmt.Println("\n--- Dados informados ---")
-	fmt.Println("Nome:", nome)
-	fmt.Println("Idade:", idade)
-	fmt.Printf("Altura: %.2f m\n", altura)
-	fmt.Println("Casada:", casada)
+    // fmt.Println("\n--- Dados informados ---")
+	// fmt.Println("Nome:", nome)
+	// fmt.Println("Idade:", idade)
+	// fmt.Printf("Altura: %.2f m\n", altura)
+	// fmt.Println("Casada:", casada)
+	fmt.Printf("Nome: %s, idade: %d, Altura: %.2f, Casada: %t\n", nome, idade, altura, casada)
 }
