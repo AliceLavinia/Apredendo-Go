053aca5391d725fe250889d860c8dac4fd7ec292
AliceLavinia <alicelhcv@gmail.com>
2025-05-15 18:49:40 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var nome string = "Alice"
+	var idade int = 22
+	var altura float64 = 1.55
+	var casada bool = true
+    
+	fmt.Println("Nome:",nome)
+	fmt.Scan(&nome)
+
+	fmt.Println("Idade: ",idade)
+    fmt.Scan(&idade)
+
+	fmt.Println("Altura: ",altura)
+	fmt.Scan(&altura)
+
+	fmt.Println("Casada: ",casada)
+	fmt.Scan(&casada)
+    
+    fmt.Println("\n--- Dados informados ---")
+	fmt.Println("Nome:", nome)
+	fmt.Println("Idade:", idade)
+	fmt.Printf("Altura: %.2f m\n", altura)
+	fmt.Println("Casada:", casada)
+}
